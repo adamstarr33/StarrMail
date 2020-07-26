30246fe34785fa6b22c922c764e4ca2a02738dfc
adamstarr33 <starrstorm1@gmail.com>
2020-07-25 20:31:47 -0400

    Added main menu to access all operations

--- StarrMail/src/StarrMail/Account.java
+++ StarrMail/src/StarrMail/Account.java
@@ -5,34 +5,45 @@ import java.util.Scanner;
 
 public class Account {
 	private String firstName, lastName, password, department, address;
-	private int capacity;
+	private int capacity = 600;
 	Scanner in = new Scanner(System.in);
 	
 	//Constructor receiving first and last name
 	Account(String first, String last)
 	{
+		starrBar();
 		this.firstName = first;
 		this.lastName = last;
 		System.out.println("Account Creation Successfull!!!!! Welcome to StarrMail, " + first + " " + last);
 		this.department = setDepartment();
 		System.out.println("You work in the " + this.department + " department");
+		starrBar();
 		System.out.println("How long of a password can you remember?\nEnter desired password length: ");
 		int length = in.nextInt();
 		this.password = generatePass(length);
-		System.out.println("Your password is " + this.password + " \nHope no one was lookin at the screen");
+		System.out.println("\nYour password is " + this.password + " \nHope no one was lookin at the screen\n");
 		System.out.println("Would you like to change your password, or is that random mess of characters doin it for you?\nEnter 1 to change password\nEnter 0 to keep password");
 		int choice = in.nextInt();
 		if(choice == 1)
 		{
-			System.out.println("What would you like your new password to be: ");
-			String pass = in.next();
-			setPass(pass);
+			setPass();
+		}
+		starrBar();
+		this.address = this.firstName.toLowerCase() + "." + this.lastName.toLowerCase() +"@" + this.department + ".starrcorp.com";
+		System.out.println("Your default address is: " + this.address + "\nDo you wanna keep that boring username, or change it to somethin a little more exciting?\nEnter 1 to change address\nEnter 0 to keep default address and be boring");
+		int switchUn = in.nextInt();
+		if(switchUn == 1)
+		{
+			System.out.println("Great choice, what do you wanna change your username to? Be creative!");
+			String un = in.next();
+			this.address = un +"@" + this.department + ".starrcorp.com";
+			System.out.println("Your new, much more exciting email address is: " + this.address);
 		}
 	}
 	
 	
 	//Prompts the user to enter which department they work in
-	private String setDepartment()
+	public String setDepartment()
 	{
 		int valid = 0;
 		do
@@ -78,16 +89,71 @@ public class Account {
 	}
 	
 	//Changes password
-	private void setPass(String pass)
+	public void setPass()
 	{
+		System.out.println("What would you like your new password to be: ");
+		String pass = in.next();
 		this.password = pass;
 		System.out.println("Your new password is " + this.password);
 	}
+	
 	//Prints a bar of starrs, you know, for pizzaz!
-	private void starrBar()
+	public void starrBar()
 	{
-		System.out.println();
+		System.out.print("\n");
 		System.out.println("********************************************");
+		System.out.print("\n");
 	}
 
+
+	
+	public int getCapacity() {
+		return capacity;
+	}
+
+
+	
+	public void setCapacity() {
+		System.out.println("What should the new capacity be: ");
+		int cap = in.nextInt();
+		this.capacity = cap;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress() {
+		System.out.println("What do you wanna change your username to? Be creative!");
+		String un = in.next();
+		this.address = un +"@" + this.department + ".starrcorp.com";
+		System.out.println("Your shiny new email address is: " + this.address);
+	}
+	
+	public void setName()
+	{
+		System.out.println("What would you like to change your name to?\nFirst name: ");
+		this.firstName = in.next();
+		System.out.println("Last name: ");
+		this.lastName = in.next();
+		System.out.println("Your new name is " + this.firstName + " " + this.lastName);
+	}
+	
+	public String getName()
+	{
+		String name = this.firstName + " " + this.lastName;
+		return name;
+	}
+	
+	public String getDepartment()
+	{
+		return this.department;
+	}
 }
+
--- StarrMail/src/StarrMail/StarrMail.java
+++ StarrMail/src/StarrMail/StarrMail.java
@@ -1,10 +1,59 @@
 package StarrMail;
 
-public class StarrMail {
+import java.util.Scanner;
+
+public class StarrMail{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Account lol = new Account("Starr", "Lord");
+		System.out.println("Welcome to StarrMail, esteemed user!\nPlease enter first and last name to begin account creation");
+		Scanner in = new Scanner(System.in);
+		System.out.println("First name: ");
+		String first = in.next();
+		System.out.println("Last name: ");
+		String last = in.next();
+		
+		Account acc = new Account(first, last);
+		int choice = 0;
+		acc.starrBar();
+		do
+		{
+			System.out.println("What would you like to do?\nEnter 1 to view your name\nEnter 2 to change your name \nEnter 3 to view your password \nEnter 4 to change your password\nEnter 5 to view your username\nEnter 6 to change username\nEnter 7 to view department\nEnter 8 to change department\nEnter 9 to view inbox capacity\nEnter 10 to change inbox capacity\nEnter 11 to exit, we'll miss you!");
+			choice = in.nextInt();
+			switch(choice)
+			{
+			case 1: 
+				acc.getName();
+				break;
+			case 2:
+				acc.setName();
+				break;
+			case 3:
+				System.out.println(acc.getPassword());
+				break;
+			case 4:
+				acc.setPass();
+			case 5:
+				System.out.println(acc.getAddress());
+				break;
+			case 6:
+				acc.setAddress();
+			case 7:
+				System.out.println(acc.getDepartment());
+				break;
+			case 8:
+				acc.setDepartment();
+				break;
+			case 9:
+				System.out.println(acc.getCapacity());
+				break;
+			case 10:
+				acc.setCapacity();
+				break;
+			case 11:
+				System.out.println("Thanks for stopping by, have a fantastic day you super starr!");
+			}
+		}while(choice!=11);
+		in.close();
 	}
 
 }
