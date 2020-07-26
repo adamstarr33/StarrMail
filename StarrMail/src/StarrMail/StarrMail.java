3ae57407903a6bf52e3ada8df3fa20e72339f25f
adamstarr33 <starrstorm1@gmail.com>
2020-07-26 00:04:19 -0400

    Fixed the display name op, added more stars :D

--- StarrMail/src/StarrMail/StarrMail.java
+++ StarrMail/src/StarrMail/StarrMail.java
@@ -18,37 +18,45 @@ public class StarrMail{
 		acc.starrBar();
 		do
 		{
+			acc.starrBar();
 			System.out.println("What would you like to do?\nEnter 1 to view your name\nEnter 2 to change your name \nEnter 3 to view your password \nEnter 4 to change your password\nEnter 5 to view your username\nEnter 6 to change username\nEnter 7 to view department\nEnter 8 to change department\nEnter 9 to view inbox capacity\nEnter 10 to change inbox capacity\nEnter 11 to exit, we'll miss you!");
 			choice = in.nextInt();
 			switch(choice)
 			{
 			case 1: 
-				acc.getName();
+				System.out.println(acc.getName());
+				System.out.println("");
 				break;
 			case 2:
 				acc.setName();
+				System.out.println("");
 				break;
 			case 3:
 				System.out.println(acc.getPassword());
+				System.out.println("");
 				break;
 			case 4:
 				acc.setPass();
 			case 5:
 				System.out.println(acc.getAddress());
+				System.out.println("");
 				break;
 			case 6:
 				acc.setAddress();
 			case 7:
 				System.out.println(acc.getDepartment());
+				System.out.println("");
 				break;
 			case 8:
 				acc.setDepartment();
 				break;
 			case 9:
 				System.out.println(acc.getCapacity());
+				System.out.println("");
 				break;
 			case 10:
 				acc.setCapacity();
+				System.out.println("");
 				break;
 			case 11:
 				System.out.println("Thanks for stopping by, have a fantastic day you super starr!");
